bf2acbcc705cae941e46e8477822bde5a455027c
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-03-23 10:13:01 +0100

    AI-Text-Summarizer

---
+++ my-app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="bg-black text-white flex justify-between items-center py-5 px-8">
+      <div>
+        <Link href="/" className="text-lg font-semibold">
+          Ztext
+        </Link>
+      </div>
+      <div>
+        <Button>Sign In</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
