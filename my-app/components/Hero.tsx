fa241cb07be11fdd609523f3b2b0182dfcd4a469
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-03-23 10:31:38 +0100

    Hero section Completed

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="px-12 flex flex-col justify-center items-center h-[85vh] relative">
+      <h1 className="text-[8vw]">AI Text Summarizer</h1>
+      <p className="text-[3vw] text-gray-700 italic mb-8">
+        Summarize text in Seconds
+      </p>
+      <Button>Get Started</Button>
 
-export default Hero
+      <div className="absolute bottom-0 left-5">
+        <p>
+          By{" "}
+          <Link
+            href="https://zgboportfolio.vercel.app"
+            target="_blank"
+            className="text-blue-500 hover:underline"
+          >
+            Emmanuel Ezeigbo
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
