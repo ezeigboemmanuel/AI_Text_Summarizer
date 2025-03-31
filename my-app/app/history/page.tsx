af623b42f259ec199d1c26d41539ec0e2d590435
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-03-31 19:53:17 +0100

    displayed recent summaries

--- my-app/app/history/page.tsx
+++ my-app/app/history/page.tsx
@@ -6,44 +6,26 @@ import { collection, doc, getDocs, query, where } from "firebase/firestore";
 import { Copy, Trash } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-const summaries = [
-  {
-    date: "7 February, 2025",
-    summary:
-      "Launched a brand-new website with improved UI/UX. Users reported faster load times and better navigation.",
-  },
-  {
-    date: "12 February, 2025",
-    summary:
-      "Crossed the 10,000 active users mark. Growth attributed to recent marketing campaigns and product improvements.",
-  },
-  {
-    date: "18 February, 2025",
-    summary:
-      "Integrated AI-powered customer support. The chatbot now handles 70% of queries, reducing response time.",
-  },
-  {
-    date: "22 February, 2025",
-    summary:
-      "Successfully hosted our annual tech conference. Over 5,000 attendees joined for insightful sessions and networking.",
-  },
-  {
-    date: "27 February, 2025",
-    summary:
-      "Released version 2.0 of our mobile app. Major updates include dark mode, improved performance, and bug fixes.",
-  },
-  {
-    date: "2 March, 2025",
-    summary:
-      "Announced a partnership with a major industry leader. This collaboration will enhance innovation and market reach.",
-  },
-];
+interface Data {
+  id: string;
+  summary: string;
+  userEmail: string;
+  userText: string;
+}
 
 const HistoryPage = () => {
   const router = useRouter();
   const { isAuth, userEmail } = useGetUserInfo();
+  const [summaries, setSummaries] = useState<Data[]>([
+    {
+      id: "",
+      summary: "",
+      userEmail: "",
+      userText: "",
+    },
+  ]);
 
   const getHistory = async () => {
     const q = query(
@@ -51,11 +33,14 @@ const HistoryPage = () => {
       where("userEmail", "==", userEmail)
     );
 
+    let summariesArray: any[] = [];
+
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
+      summariesArray.push(doc.data());
     });
+
+    setSummaries(summariesArray);
   };
 
   useEffect(() => {
@@ -79,7 +64,13 @@ const HistoryPage = () => {
             <Link href={"/summary/123"}>
               <div>
                 <div className="flex justify-between mb-4 text-gray-500 text-sm">
-                  <p>{summary.date}</p>
+                  <p>
+                    {new Date(Number(summary.id)).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </p>
 
                   <div className="flex items-center space-x-4">
                     <Copy className="w-5 h-5" />
@@ -87,7 +78,7 @@ const HistoryPage = () => {
                   </div>
                 </div>
                 <div>
-                  <p>{summary.summary}</p>
+                  <p className="line-clamp-3">{summary.summary}</p>
                 </div>
               </div>
             </Link>
